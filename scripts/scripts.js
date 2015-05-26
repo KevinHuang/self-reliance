7f75a28e140093aaa4f80a7c73a2a34c1f925a34
黃文龍 <wlhu.nag+gi@gmail.com>
2015-05-26 15:07:10 +0800

    Add Facebook login

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -64,37 +64,102 @@ angular.module("yapp", ["firebase", "ui.router", "ngAnimate", "ui.bootstrap", "n
     }])
 
 
-    .controller("LoginCtrl", ["$scope", "$location", "$firebaseAuth", function ($scope, $location,$firebaseAuth) {
-        var ref = new Firebase("https://amber-heat-6612.firebaseio.com");
-        var auth = $firebaseAuth(ref);
-        // login with Facebook
-        auth.$authWithOAuthPopup("facebook").then(function(authData) {
-            console.log("Logged in as:", authData.uid);
-        }).catch(function(error) {
-            console.log("Authentication failed:", error);
-        });
-        $scope.submit = function () {
-            console.log('Thsi is loginctrl!')
-            return $location.path("/dashboard"), !1
-        }
-    }])
+    .controller("LoginCtrl", ["$scope", "$state", "$rootScope",
+        function ($scope, $state, $rootScope) {
+            $scope.$state = $state;
+            var ref = new Firebase("https://amber-heat-6612.firebaseio.com");
+            var authData = ref.getAuth();
+            $scope.authData = authData;
+
+            //if (Auth.getAuth()) {
+            //    $state.go('job');
+            //}
+
+            var isNewUser = true;
+
+            function getName(authData) {
+                switch (authData.provider) {
+                    case 'password':
+                        return authData.password.email.replace(/@.*/, '');
+                    case 'twitter':
+                        return authData.twitter.displayName;
+                    case 'facebook':
+                        return authData.facebook.displayName;
+                }
+            };
+
+            $scope.testlogin = function () {
+
+                if (authData) {
+                    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+                    console.dir(authData);
+                    $scope.authdata = authData;
+                    var ref = new Firebase("https://amber-heat-6612.firebaseio.com");
+                    ref.child("users").child(authData.uid).set({
+                        provider: authData.provider,
+                        name: getName(authData)
+                    });
+                } else {
+                    console.log("User is logged out");
+                }
+                ;
+
+            };
+
+            $scope.testlogout = function () {
+                console.log("Try log out now!")
+                ref.unauth();
+                $scope.testlogin();
+            };
+
+            // login with Facebook
+            $scope.loginfb = function () {
+                console.log('This is loginctrl!');
+                ref.authWithOAuthRedirect("facebook", function (error) {
+                    if (error) {
+                        console.log("Login failed!", error);
+                    } else {
+                        console.log("Login success!");
+                    }
+                });
+            };
+
+        }])
 
     .controller("DashboardCtrl", ["$scope", "$state", function ($scope, $state) {
-        $scope.$state = $state
+        $scope.$state = $state;
+        var ref = new Firebase("https://amber-heat-6612.firebaseio.com");
+        var authData = ref.getAuth();
+        if (authData) {
+
+            $scope.authData = authData;
+        } else {
+            $state.go('login');
+        }
+        $scope.logout = function () {
+            ref.unauth();
+        };
+
     }])
 
 
     .controller("ProfileCtrl", ["$scope", "$state", "$firebaseObject", function ($scope, $state, $firebaseObject) {
         $scope.$state = $state;
-        var ref = new Firebase("https://amber-heat-6612.firebaseio.com/user");
+        var ref = new Firebase("https://amber-heat-6612.firebaseio.com");
+        var authData = ref.getAuth();
+        $scope.authData = authData;
+
+        var nref = ref.child("users").child(authData.uid);
 
         // create a synchronized array
-        $scope.user = $firebaseObject(ref);
+        $scope.user = $firebaseObject(nref);
 
         console.log($scope.user);
         $scope.saveProfile = function () {
-            $scope.user.$save({
-                text: $scope.user
+            $scope.user.$save().then(function (nref) {
+                nref.key() === $scope.user.$id; // true
+            }, function (error) {
+                console.log("Error:", error);
             });
         }
     }])
@@ -170,7 +235,25 @@ angular.module("yapp", ["firebase", "ui.router", "ngAnimate", "ui.bootstrap", "n
         console.log($scope.entry.salary1);
     }])
 
-    .controller("JobCtrl", ["$scope", "$firebaseArray", function ($scope, $firebaseArray) {
-        var ref = new Firebase("https://amber-heat-6612.firebaseio.com/jobs");
-        $scope.jobs = $firebaseArray(ref);
-    }]);
+    .controller("JobCtrl", ["$scope", "$firebaseArray", "$firebaseAuth",
+        function ($scope, $firebaseArray, $firebaseAuth) {
+            var ref = new Firebase("https://amber-heat-6612.firebaseio.com/jobs");
+            $scope.jobs = $firebaseArray(ref);
+            var ref = new Firebase("https://amber-heat-6612.firebaseio.com");
+
+            var authData = ref.getAuth();
+            $scope.authdata = authData;
+            $scope.testlogin = function () {
+
+
+                if (authData) {
+                    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+                    console.dir(authData);
+                    $scope.authdata = authData;
+                } else {
+                    console.log("User is logged out");
+                }
+                ;
+
+            };
+        }]);
